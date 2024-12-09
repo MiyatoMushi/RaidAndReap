ad9275f41f55808395f37d12540aadd4693eca40
jervenscabanilla <jervensc@gmail.com>
2024-12-10 01:23:25 +0800

    Main Menu 👽

--- Raid & Reap/Assets/Scripts/MainMenuButtons.cs
+++ Raid & Reap/Assets/Scripts/MainMenuButtons.cs
@@ -1,33 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenuButtons : MonoBehaviour
 {
     public AudioSource mainMenuButtonSound;
+    public GameObject canvas;
 
-    public void NewButton() {
+    public void NewButton()
+    {
         mainMenuButtonSound.Play();
         SceneManager.LoadScene("PlayerFarm");
     }
-
     public void LoadButton() {
         mainMenuButtonSound.Play();
     }
 
     public void ExitButton() {
         mainMenuButtonSound.Play();
+        Application.Quit();
+        Debug.Log("Exit button clicked!");
     }
 
     public void AboutButton() {
         mainMenuButtonSound.Play();
+        canvas.SetActive(false);
     }
 
     public void SettingsButton() {
         mainMenuButtonSound.Play();
+        canvas.SetActive(false);
     }
 
+    public void ReturnButton() {
+        mainMenuButtonSound.Play();
+        canvas.SetActive(false);
+    }
     
 
     /* Start is called before the first frame update
